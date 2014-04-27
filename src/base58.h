6b1f27032c18d2d634704688a294e7af055905df
roulettecoin <roulettecoin@gmail.com>
2014-04-27 13:36:16 +0200

    Changed address prefixes to T for main network and t for testnet.

--- src/base58.h
+++ src/base58.h
@@ -272,9 +272,9 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Roulettecoin addresses start with L
+        PUBKEY_ADDRESS = 66, // Roulettecoin addresses start with T
         SCRIPT_ADDRESS = 5,
-        PUBKEY_ADDRESS_TEST = 111,
+        PUBKEY_ADDRESS_TEST = 127,
         SCRIPT_ADDRESS_TEST = 196,
     };
 
