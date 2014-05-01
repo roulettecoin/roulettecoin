3b57ed6ce7a24d9d3b6d727d5b46a021e1dad07f
roulettecoin <roulettecoin@gmail.com>
2014-05-01 14:19:08 +0200

    Changed coin ticker symbol.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("RLT");
+    case mBTC: return QString("mRLT");
+    case uBTC: return QString::fromUtf8("μRLT");
     default: return QString("???");
     }
 }
